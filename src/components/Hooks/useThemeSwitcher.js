39985a86f983c357d1a7a250c071807990b3abaf
woo49m <i861128@gmail.com>
2023-06-03 13:17:42 +0800

    設定深色主題2

--- src/components/Hooks/useThemeSwitcher.js
+++ src/components/Hooks/useThemeSwitcher.js
@@ -5,48 +5,11 @@ const useThemeSwitcher = () => {
   const [mode, setMode] = useState("dark");
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia(PreferDarkQuery);
-    const userPref = window.localStorage.getItem("theme");
-    console.log("this is local theme: " + userPref);
-
-    const handleChange = () => {
-      if (userPref) {
-        let check = userPref === "dark" ? "dark" : "light";
-        console.log("check: " + check);
-        setMode(check);
-        console.log("mode: " + mode);
-        if (check === "dark") {
-          document.documentElement.classList.add("dark");
-        } else {
-          document.documentElement.classList.remove("dark");
-        }
-      } else {
-        /*let check = mediaQuery.matches ? "dark" : "light";*/
-        let check = "dark";
-        setMode(check);
-        if (check === "dark") {
-          document.documentElement.classList.add("dark");
-        } else {
-          document.documentElement.classList.remove("dark");
-        }
-      }
-    };
-
-    handleChange();
-
-    mediaQuery.addEventListener("change", handleChange);
-
-    return () => mediaQuery.removeEventListener("change", handleChange);
-  }, []);
-
-  useEffect(() => {
     console.log("2 Effect: " + mode);
     if (mode === "dark") {
-      window.localStorage.setItem("theme", "dark");
       document.documentElement.classList.add("dark");
     }
     if (mode === "light") {
-      window.localStorage.setItem("theme", "light");
       document.documentElement.classList.remove("dark");
     }
   }, [mode]);
